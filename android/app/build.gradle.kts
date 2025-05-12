2a428093d094644b19623ec5cea1fc8dc8dc0daa
Chandu Gangola <chandugangola444@gmail.com>
2025-05-13 01:31:18 +0530

    changes on build.grade.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.spacex_app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
 //    ndkVersion = flutter.ndkVersion
     ndkVersion = "27.0.12077973"
 
@@ -25,8 +25,8 @@ android {
         applicationId = "com.example.spacex_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 21
+        targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
